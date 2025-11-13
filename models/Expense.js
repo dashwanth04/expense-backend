bf1863fc4e0933856a6f6065de54ae83bad183b5
dashwanth_ram <ramchowdary640@gmail.com>
2025-11-13 07:05:21 +0530

    1

--- models/Expense.js
+++ models/Expense.js
@@ -1,22 +1,28 @@
 import mongoose from "mongoose";
 
-const expenseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const expenseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount is required"],
+      min: [1, "Amount must be greater than 0"],
+    },
+    category: {
+      type: String,
+      default: "General",
+      trim: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  amount: {
-    type: Number,
-    required: true
-  },
-  category: {
-    type: String,
-    default: "General"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Expense", expenseSchema);
